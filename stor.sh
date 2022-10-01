2f4c2c87923149c9eee1c8d756e5a5cf413626d0
Tim-ksvr <102039667+Tim-ksvr@users.noreply.github.com>
2022-10-01 20:04:57 +0800

    Update stor.sh

--- stor.sh
+++ stor.sh
@@ -1,4 +1,3 @@
 #/bin/sh
 
-/app/gh -L=ss+mws://$METHOD:$PASSWORD@localhost:3580 >/dev/null &
-nginx -g daemon off
+/app/gh -L=ss+mws://$METHOD:$PASSWORD@localhost:3580 >/dev/null;nginx -g "daemon off"
